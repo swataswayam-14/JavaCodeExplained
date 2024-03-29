c646441c78d73285be2af4a105e1bfb195dad3c9
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-03-29 11:28:21 +0530

    first and last position of element solved in linear time

--- BinarySearchInterview/FindFirstAndLastPositionOfElementInSortedArray.java
+++ BinarySearchInterview/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -3,27 +3,45 @@ package BinarySearchInterview;
 import java.util.*;
 public class FindFirstAndLastPositionOfElementInSortedArray {
     public static void main(String[] args) {
-        int [] nums = {1,2,3,3,4,5};
-        searchRange(nums,3, 0, nums.length-1);
-        
+        int [] nums = {1,2,3,3,3,3,4,4,5};
+        System.out.println(Arrays.toString(searchRangelinear(nums, 2)));
     }
-    static ArrayList<Integer> ans = new ArrayList<>();
 
-    static void searchRange(int[] nums , int target, int s , int e){
-        if(s>=e){
-            int m = s + (e-s)/2;
 
-            if(nums[m] == target){
-                ans.add(m);
+    static int[] searchRangelinear(int[] nums , int target){
+        int[] range = new int[nums.length];
+        int occurence = 0;
+        int index = 0;
+        int size = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(target == nums[i]){
+                size++;
             }
-            else if(nums[m] > target){
-                searchRange(nums, target, s, m-1);
-            }else{
-                searchRange(nums, target, m+1, e);
+            if(target == nums[i] && occurence <1){ //when the target first occurs then the if condition runs
+                range[i] = i; //the target is found at index i , hence we store the index of the target in the range array
+                index = i;
+                occurence++; //the target has been found once (first occurence) 
             }
-        }else{
-            System.out.println(ans);
-            return;
         }
-    }
+        for (int i = index; i < nums.length; i++) {
+            if(target != nums[i] && target == nums[i-1]){ //1,2,3,3,4
+                range[i-1] = i-1; 
+            }
+        }
+        if(size > 1){
+            size = 2;
+        }else{
+            size = 1;
+        }
+        int[] ans = new int[size];
+        int k = 0;
+        for (int i = 0; i < range.length; i++) {
+            if(range[i] != 0){
+                ans[k] = range[i];
+                k++;
+            }
+        }
+        return ans;
+    } 
+    
 }
