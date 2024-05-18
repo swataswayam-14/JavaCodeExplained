011a96187362ac8442c1e6d7c86fb08a865dd016
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-05-19 00:15:57 +0530

    instanceof method codes added

--- AccessControl/SubClass.java
+++ AccessControl/SubClass.java
@@ -8,6 +8,11 @@ public class SubClass extends A {
         SubClass obj = new SubClass(0, null, 0);
         int n = obj.sum; //protected data members can be accessed by subclasses present in the same package or in the subclass of different package
         //but protected data members cannot be accessed in different package which is not subclass
+
+
+        System.out.println(obj instanceof A);  //obj is a Object of the class SubClass which extends A hence its also an instance of A : output : true
+        System.out.println(obj instanceof SubClass);
+        System.out.println(obj instanceof Object);
     }
 }
 
