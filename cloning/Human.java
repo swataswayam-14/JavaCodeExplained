e3fd302ce0636c8b73857bacb955904d4bd10461
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-05-22 23:37:12 +0530

    shallow and deep copy of objects code added

--- cloning/Human.java
+++ cloning/Human.java
@@ -3,18 +3,30 @@ package cloning;
 public class Human implements Cloneable{
     String name;
     int age;
+    int arr[];
     public Human(String name, int age){
         this.name = name;
         this.age = age;
+        this.arr = new int[]{1,2,3,4,5,6};
     }
 
     public Human(Human other){
         this.name = other.name;
         this.age = other.age;
+        this.arr = other.arr;
     }
 
     public Object clone() throws CloneNotSupportedException{
         return super.clone(); //shalow copy of the object
+    }
+
+    public Object DeepClone() throws CloneNotSupportedException{
+        Human twin = (Human)super.clone(); //shallow copy
+        twin.arr = new int[this.arr.length];
+        for (int i = 0; i < twin.arr.length; i++) {
+            twin.arr[i] = this.arr[i];
+        }
+        return twin;
     }
 
 }
--- cloning/Main.java
+++ cloning/Main.java
@@ -1,5 +1,7 @@
 package cloning;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException{
         Human swayam = new Human("swayam", 20);
@@ -8,5 +10,19 @@ public class Main {
 
         Human twinMoreOverhead = new Human(swayam); //in efficient : more program overhead because of new keyword
         System.out.println(twinMoreOverhead.age);
+
+        System.out.println(Arrays.toString(twin.arr)); //its pointing to the same object , hence any change in the twin will be reflected in the original arr
+        twin.arr[0] = 10000;
+        System.out.println(Arrays.toString(swayam.arr)); 
+
+
+        //deepTwin is a deep copy of swayam
+        Human deepTwin = (Human)swayam.DeepClone();
+        deepTwin.arr[0] = 121212;
+        System.out.println(Arrays.toString(swayam.arr));
+        System.out.println(Arrays.toString(deepTwin.arr));
+
+        deepTwin.name = "deep twin";
+        System.out.println(swayam.name);
     }
 }
