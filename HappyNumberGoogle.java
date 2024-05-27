85fd4e37b8d8bf2ececff7a79ee724078f617b85
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-05-27 13:07:24 +0530

    2 pointer approach for solving the happy number problem code addded

--- HappyNumberGoogle.java
+++ HappyNumberGoogle.java
@@ -25,7 +25,35 @@ public class HappyNumberGoogle {
             temp = sum;
         }
     }
+
+    public static boolean isHappy(int num){
+        int slow = num;
+        int fast = num;
+    
+        do {
+            slow = findSquare(slow);
+            fast = findSquare(findSquare(fast));
+        } while (slow != fast && slow != 1);
+    
+        if(slow == 1){
+            System.out.println("The number " + num + " is a happy number");
+            return true;
+        }
+        System.out.println("The number " + num + " is not a happy number");
+        return false;
+    }
+    
+    private static int findSquare(int num){
+        int square = 0;
+        while (num != 0) {
+            int l = num % 10;
+            square += (l * l);
+            num = num / 10;
+        }
+        return square;
+    }
     public static void main(String[] args) {
         detect(91);
+        System.out.println(isHappy(19));
     }
 }
